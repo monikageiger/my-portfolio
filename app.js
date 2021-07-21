856f048257abcd438f7b5e636c7170b5b1dcdaa2
Monika Geiger <geigermonika@hotmail.hu>
2021-07-21 16:46:09 +0200

    basic page shower

---
+++ app.js
@@ -0,0 +1,28 @@
+let campgrPage = document.getElementById('campgrPage')
+campgrPage.onclick = function () {
+    window.open('https://limitless-sands-52333.herokuapp.com/');
+}
+
+
+let yahtzeePage = document.getElementById('yahtzeePage')
+yahtzeePage.onclick = function () {
+    window.open('https://monikageiger.github.io/yahtzee-game-react/');
+}
+
+
+let pokemonPage = document.getElementById('pokemonPage')
+pokemonPage.onclick = function () {
+    window.open('https://monikageiger.github.io/pokemon-images-displayer/');
+}
+
+
+let pingpongPage = document.getElementById('pingpongPage')
+pingpongPage.onclick = function () {
+    window.open('https://monikageiger.github.io/ping-pong-score-keeper/');
+}
+
+
+let npmPage = document.getElementById('npmPage')
+npmPage.onclick = function () {
+    window.open('https://monikageiger.github.io/lab-html-exercise/');
+}
